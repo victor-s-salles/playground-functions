ceaaf5832e03f74518d498de674baf51bec50401
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-26 15:05:32 -0300

    Resolvido o requisito 3 do projeto

--- src/challenges.js
+++ src/challenges.js
@@ -11,8 +11,9 @@ function calcArea(base, altura) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(palavra) {
+  let separada = palavra.split(' ');
+  return separada;
 }
 
 // Desafio 4
