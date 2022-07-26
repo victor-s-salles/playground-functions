0dd791dc1f78273b789895ae95f7b54767dd8966
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-26 20:04:26 -0300

    Feito o requisito 11 do projeto

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,51 @@
 // Desafio 11
-function generatePhoneNumber() {
+function testa3(number) {
+  let valor = 0;
+  let cont = 0;
+  for (let index in number) {
+    valor = number[index];
+    cont = 0;
+    for (let index2 in number) {
+      if (valor === number[index2]) {
+        cont += 1;
+      }
+    }
+    if (cont >= 3) {
+      return true;
+    }
+  }
 
+  return false;
+}
+
+function generatePhoneNumber(number) {
+  let formated = [];
+
+  if (number.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let index in number) {
+    if (number[index] < 0 || number[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  if (testa3(number)) { return 'não é possível gerar um número de telefone com esses valores'; }
+
+  formated.push('(');
+  formated.push(number[0]);
+  formated.push(number[1]);
+  formated.push(')');
+  formated.push(' ');
+
+  for (let index = 2; index <= 6; index += 1) { formated.push(number[index]); }
+  formated.push('-');
+
+  for (let index = 7; index <= 10; index += 1) { formated.push(number[index]); }
+
+  formated = formated.join('');
+
+  return formated;
 }
 
 // Desafio 12
@@ -20,7 +65,7 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate() {
-  
+
 }
 
 module.exports = {
